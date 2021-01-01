766b3ec19c79d0248ae40be664f22903b6f53f80
bunino <matteo.bunino@eurecom.fr>
2021-01-01 17:57:54 +0100

    deploy 1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCCi8Bq6giEl6JsYAjMRB-Q92cFaeUsqzY",
+    authDomain: "covidapp-451c6.firebaseapp.com",
+    projectId: "covidapp-451c6",
+    storageBucket: "covidapp-451c6.appspot.com",
+    messagingSenderId: "599602287370",
+    appId: "1:599602287370:web:de2c022ec88c64fe1920f3",
+    measurementId: "G-7LSJL6DG3E"
+  }
 };
